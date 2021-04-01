85a9e24334af13ad13c450046cd7ac8cfc65463b
James Shopland <github@jamesshopland.com>
2021-04-01 22:35:47 +0000

    Create pac-initial.sh

---
+++ pac-initial.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run -it \
+--name=plex-auto-collections  \
+-e PGID=1001 -e PUID=1000  \
+--label com.github.cloudbox.cloudbox_managed=true \
+--network=cloudbox  \
+-v /opt/plex-auto-collections:/config \
+mza921/plex-auto-collections -c $1
