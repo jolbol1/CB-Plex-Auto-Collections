c95aefc443775d91c9103dc443b48f268770f85b
jolbol1 <github@jamesshopland.com>
2021-04-02 00:54:09 +0100

    Change mount to /opt/plex-auto-collections/config to allow for multi config set ups

--- pac-headless.sh
+++ pac-headless.sh
@@ -4,5 +4,5 @@ docker run -d --rm \
 -e PGID=1001 -e PUID=1000  \
 --label com.github.cloudbox.cloudbox_managed=true \
 --network=cloudbox  \
--v /opt/plex-auto-collections:/config \
+-v /opt/plex-auto-collections/config:/config \
 mza921/plex-auto-collections -u -c $1
